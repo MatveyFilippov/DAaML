ad605cdd8aff5ea5abd8a2d19746f1c2834dd105
MatveyFilippov <matdufi2004@gmail.com>
2024-12-16 00:03:27 +0300

    add MAE

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data_processor import test_data
 
 
 m = model.get_model()
+print(f'Mean Absolute Error: {model.get_mae():.2f}')
 d = test_data.get_data(rooms=3)
 p = m.predict(d)
 print(d)
--- model.py
+++ model.py
@@ -21,16 +21,18 @@ def __get_data_to_train() -> tuple:
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-def fit_model() -> str:
+def fit_model():
     X_train, X_test, y_train, y_test = __get_data_to_train()
     model = LinearRegression()
     model.fit(X_train, y_train)
-
     __MODEL.object = model
 
+
+def get_mae() -> float:
+    _, X_test, _, y_test = __get_data_to_train()
+    model = get_model()
     y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    return f'Mean Absolute Error: {mae:.2f}'
+    return mean_absolute_error(y_test, y_pred)
 
 
 def get_model() -> LinearRegression:
