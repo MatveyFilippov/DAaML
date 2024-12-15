26775a4e730aa45da77b3299083f0bfb68fb9830
MatveyFilippov <matdufi2004@gmail.com>
2024-12-16 00:38:14 +0300

    remove model analysis

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from data_processor import test_data
 
 
 m = model.get_model()
-print(f'Mean Absolute Error: {model.get_mae():.2f}')
 d = test_data.get_data(rooms=3)
 p = m.predict(d)
 print(d)
--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
 import data_processor
 import settings
 from data_processor import train_data
@@ -12,7 +11,7 @@ from data_processor import train_data
 __MODEL = data_processor.ImportableObject("main_ml_model")
 
 
-def __get_data_to_train() -> tuple:
+def get_data_to_train_and_test() -> tuple:
     data = train_data.get_data()
 
     X = data[settings.FEATURE_COLUMNS]
@@ -22,17 +21,10 @@ def __get_data_to_train() -> tuple:
 
 
 def fit_model():
-    X_train, X_test, y_train, y_test = __get_data_to_train()
+    X_train, _, y_train, _ = get_data_to_train_and_test()
     model = LinearRegression()
     model.fit(X_train, y_train)
     __MODEL.object = model
-
-
-def get_mae() -> float:
-    _, X_test, _, y_test = __get_data_to_train()
-    model = get_model()
-    y_pred = model.predict(X_test)
-    return mean_absolute_error(y_test, y_pred)
 
 
 def get_model() -> LinearRegression:
