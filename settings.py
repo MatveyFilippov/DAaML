73ecea24b12e33db2013322303c3a2f7f3679d04
MatveyFilippov <matdufi2004@gmail.com>
2024-12-16 00:23:23 +0300

    add comments to column names

--- settings.py
+++ settings.py
@@ -3,13 +3,13 @@ PATH_TO_TEST_DATA = 'house-pricing-prediction/houses_test_data.csv'
 PATH_TO_OBJS_DIR = "Objects"
 
 FEATURE_COLUMNS = [
-    'Rooms',
-    'Distance',
-    'Bedroom2',
-    'Bathroom',
-    'Car',
-    'Landsize',
-    'BuildingArea',
-    'YearBuilt'
+    'Rooms',  # количество комнат в доме
+    'Distance',  # расстояние от дома до центра города
+    'Bedroom2',  # количество спален в доме
+    'Bathroom',  # количество ванных комнат в доме
+    'Car',  # количество парковочных мест
+    'Landsize',  # размер участка
+    'BuildingArea',  # жилая площадь
+    'YearBuilt'  # год постройки
 ]
-TARGET_COLUMN = 'Price'
+TARGET_COLUMN = 'Price'  # цена на дом
